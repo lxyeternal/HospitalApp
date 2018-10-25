0fa9d2f23c5b6947da9f5f9b432c2ebb633f17d5
honywen <1796369212@qq.com>
2018-10-25 21:42:49 +0800

    2018-10-25
    God bless my code!

---
+++ app/src/main/java/com/example/guoxiaowen/hospital/AppointmentActivity.java
@@ -0,0 +1,21 @@
+package com.example.guoxiaowen.hospital;
+
+import  android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uploading1);
+        initView();
+
+    }
+
+    private void initView() {
+
+    }
+}
+
---
+++ app/src/main/java/com/example/guoxiaowen/hospital/Healthmanage.java
@@ -0,0 +1,20 @@
+package com.example.guoxiaowen.hospital;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Healthmanage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uploading1);
+        initView();
+
+    }
+
+    private void initView() {
+
+    }
+}
